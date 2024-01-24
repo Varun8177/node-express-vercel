d8c2f5471b818b4c840fa23339cd21d276b6c281
Varun Ergurala <112754116+Varun8177@users.noreply.github.com>
2024-01-24 14:55:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/home", home);
+app.use("/home", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
+});
 
 // connection
-const port = process.env.PORT || 9001;
+const port =  9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
